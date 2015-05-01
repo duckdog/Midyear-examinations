316fd14da5fd33326b2257fd08673666728b44ee
takumi_sato <biker-duck.g@ezweb.ne.jp>
2015-05-01 21:50:05 +0900

    個人用です.

---
+++ common.h
@@ -0,0 +1,17 @@
+
+//
+//  cmmon.h
+//  MawasuHito
+//
+//  Created by 佐藤 匠 on 2015/05/01.
+//
+//
+
+#pragma once
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+#include "cinder/app/AppNative.h"
+#include "cinder/gl/gl.h"

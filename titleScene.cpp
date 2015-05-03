9cdc42e8a31468321aac3a42272591c77a500745
takumi_sato <biker-duck.g@ezweb.ne.jp>
2015-05-03 17:24:38 +0900

    repair now
    study

--- titleScene.cpp
+++ titleScene.cpp
@@ -7,3 +7,7 @@
 //
 
 #include "titleScene.hpp"
+
+void titleScene::draw(){
+    gl::clear(Color(0,1,1));
+}
--- titleScene.hpp
+++ titleScene.hpp
@@ -13,14 +13,11 @@
 #include "common.h"
 #include "sceneBase.hpp"
 
-class sceneBase;
 
 class titleScene : public sceneBase{
        
 public:
     void draw(){}
-    void update(){
-    gl::clear(Color(1,0,1));
-    }
+    void update(){}
 };
 

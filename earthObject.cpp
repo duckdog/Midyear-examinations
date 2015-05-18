34273ff0091f1cbfb7fcd677ab1aef98ed256841
takumi_sato <biker-duck.g@ezweb.ne.jp>
2015-05-19 01:27:59 +0900

    ちきゅう（立方体）を描画。

---
+++ earthObject.cpp
@@ -0,0 +1,9 @@
+//
+//  earthObject.cpp
+//  MAWASU_IOS
+//
+//  Created by 佐藤 匠 on 2015/05/18.
+//
+//
+
+#include "earthObject.h"
---
+++ earthObject.h
@@ -0,0 +1,14 @@
+//
+//  earthObject.h
+//  MAWASU_IOS
+//
+//  Created by 佐藤 匠 on 2015/05/18.
+//
+//
+
+#ifndef __MAWASU_IOS__earthObject__
+#define __MAWASU_IOS__earthObject__
+
+#include <stdio.h>
+
+#endif /* defined(__MAWASU_IOS__earthObject__) */

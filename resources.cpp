485ea41dcacfda403cf2cb43ba3f8055456e519c
takumi_sato <biker-duck.g@ezweb.ne.jp>
2015-05-19 18:38:28 +0900

    res描画設定の変更. changed drawing system.

--- resources.cpp
+++ resources.cpp
@@ -1,32 +1,58 @@
 
 #include "resources.h"
 
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <cmath>
 
 
-resourceManage::resourceManage(){
+resourceManage::resourceManage():
+mSurfaceComposite(getWindowWidth(),getWindowHeight(),true){
    }
 
+
+//画像取得
 gl::Texture resourceManage::getsprite(const SpriteID id){
     auto it = m_graph.find(id);
     return it->second;
 }
 
+//リソースクラスの取得
 resourceManage& resourceManage::getinstace(){
     static resourceManage resource;
     return resource;
 }
 
+
+//追加（そのまま）
 void resourceManage::add(const SpriteID& m_id,const std::string& m_pass){
     
     m_graph.insert
     (std::make_pair(m_id,gl::Texture(loadImage(loadResource(m_pass)))));
 }
+//追加（画像リサイズ用）
+void resourceManage::add(const SpriteID& m_id,const std::string& m_pass,
+                         const Area use_area,const Area pos_area){
+    
+    //画像のリサイズ処理.
+    Surface imageSurface(loadImage(loadResource(m_pass)));
+    ci::ip::resize(imageSurface,use_area,&mSurfaceComposite,
+                   pos_area,FilterSincBlackman());
+    
+    m_graph.insert
+    (std::make_pair(m_id,gl::Texture(mSurfaceComposite)));
+}
 
+
+//個別消去
 void resourceManage::remove(const SpriteID id){
     auto it = m_graph.find(id);
     m_graph.erase(it);
     
 }
+
+//完全消去
 void resourceManage::clear(){
     m_graph.clear();
 }
--- resources.h
+++ resources.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "common.h"
+#include "cinder/Surface.h"
+#include "cinder/ip/Resize.h"
 #include <map>
 
 enum SpriteID{
@@ -20,10 +22,17 @@ private:
     std::map<SpriteID, gl::Texture> m_graph;//:キーを指定、テクスチャを保持
     
 public:
+    
+    //画像情報宣言用の変数.
+    Surface mSurfaceComposite;
     SpriteID m_id;
     std::string m_pass;
+    
     static resourceManage& getinstace();
     void add(const SpriteID&,const std::string&);
+    void add(const SpriteID&,const std::string&,
+             const Area,const Area);
+
     void remove(const SpriteID);
     void clear();
     

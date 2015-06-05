e6499a0307589331d1f8f9ee81a4e48ef80a0051
takumi_sato <biker-duck.g@ezweb.ne.jp>
2015-06-05 21:20:39 +0900

    小人（オブジェクト）の出現リミット、地球が何周したかを表示.

--- drawNumber.cpp
+++ drawNumber.cpp
@@ -22,9 +22,15 @@ std::string differentFont( "Papyrus" );
     
   m_fontsize = 20;
   m_font = Font(boldFont, m_fontsize);
-  m_pos =  Vec2f( 70 - ajust_pos, -getWindowHeight()/2 +  55);
+  m_rotepos =  Vec2f( 70 - ajust_pos, -getWindowHeight()/2 +  55);
+  m_rimitpos =  Vec2f(70, -getWindowHeight()/2 +  25);
+
   rote_count = ("null_string");
   is_carry = false;
+  is_rim_carry = false;
+    
+  ajustment = 1;
+  rim_ajustment = 1;
 }
 
 
@@ -36,23 +42,28 @@ drawNumberSP drawNumber::getinstance(){
 }
 
 
-void drawNumber::draw(){
-    
-    
-    drawupdate(1,1);
+void drawNumber::drawrotecount(){
+
     gl::pushModelView();
-    gl::drawString(rote_count,m_pos,
+    gl::drawString(rote_count,m_rotepos,
                    ColorA(0,0,0,1),m_font);
-//    gl::drawString(kobito_count,m_pos,
-//                ColorA(0,1,1,1),m_font);
     gl::popModelView();
-    /*gl::draw(rotate_count_texture ,m_pos);
-    gl::draw(kobito_count_texture ,m_pos + Vec2f(0,-m_fontsize));
-    
-    gl::popModelView();*/
+ 
 }
 
-void drawNumber::drawupdate(int rotation,int survivor_amount){
+
+void drawNumber::drawrimitcount(){
+    
+    gl::pushModelView();
+    gl::drawString(kobito_count,m_rimitpos,
+                   ColorA(0,0,0,1),m_font);
+    gl::popModelView();
+}
+
+
+
+
+void drawNumber::drawroteupdate(){
     
     if((*object::earth_date())->m_rotation  >=  360  &&
        static_cast<int>((*object::earth_date())->m_rotation / 360) % ajustment == 0){
@@ -62,7 +73,7 @@ void drawNumber::drawupdate(int rotation,int survivor_amount){
     }
     //位が上がったら、左横にずらして調整.
     if(is_carry){
-        m_pos.x -=  (m_fontsize/2);
+        m_rotepos.x -=  (m_fontsize/2);
         ajustment *= 10;
         leverage++;
         is_carry = false;
@@ -92,6 +103,27 @@ void drawNumber::drawupdate(int rotation,int survivor_amount){
 */
 
 }
+
+// 現在小人数,リミットを描画.
+void drawNumber::drawrimitupdate(int current_amount,int rimit_survivor_amount){
+
+    if(current_amount != 0 && current_amount % (10 * rim_ajustment) == 0){
+        console() << current_amount % 10 * rim_ajustment;
+        is_rim_carry = true;
+        
+    }
+    //位が上がったら、左横にずらして調整.
+    if(is_rim_carry){
+        m_rimitpos.x -=  (m_fontsize/2);
+        rim_ajustment *= 10;
+        rim_leverage++;
+        is_rim_carry = false;
+    }
+
+    kobito_count = (toString(current_amount)+(" / ")+toString(rimit_survivor_amount));
+    
+}
+
 
 
 
--- drawNumber.h
+++ drawNumber.h
@@ -1,8 +1,8 @@
-
+#pragma once
 #include "common.h"
 #include "resources.h"
 #include "cinder/Font.h"
-#include "earthObject.h"
+//#include "earthObject.h"
 
 class drawNumber;
 typedef std::unique_ptr<drawNumber> drawNumberSP;
@@ -10,8 +10,11 @@ typedef std::unique_ptr<drawNumber> drawNumberSP;
 class drawNumber{
 public:
     drawNumber();
-    void draw();
-    void drawupdate(int rotation,int survivor_amount);
+    void drawrotecount();
+    void drawrimitcount();
+    
+    void drawroteupdate();
+    void drawrimitupdate(int survivor_amount ,int amount_rimit);
     void fontname();
     static drawNumberSP getinstance();
     
@@ -26,12 +29,18 @@ private:
     std::string rote_count;
     std::string kobito_count;
     
-    Vec2f m_pos;
+    Vec2f m_rotepos;
+    Vec2f m_rimitpos;
     Font m_font;
     int m_fontsize;
     bool is_carry;
     int ajust_pos = 0;
     int ajustment = 10;
     int leverage  = 1;
+    
+    bool is_rim_carry;
+    int rim_ajust_pos = 0;
+    int rim_ajustment = 10;
+    int rim_leverage  = 1;
     
 };

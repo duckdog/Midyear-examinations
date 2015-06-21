71aa46d110bdf50a6526aa69238934e168f2cf03
takumi_sato <biker-duck.g@ezweb.ne.jp>
2015-06-21 18:39:19 +0900

    チュートリアル用のシーンを設定.テスト中.

--- tutorialScene.cpp
+++ tutorialScene.cpp
@@ -3,46 +3,77 @@
 
 TutorialScene::TutorialScene(std::shared_ptr<sceneManage> manage) :
 sceneBase(manage),
-m_ids{SpriteID::ALL_sprite},
-m_passes{"NULL.png"}{
-    is_touch = false;
-    current_text_number = 0;
-    
-}
+m_intordaction_ids{SpriteID::introdaction_0,SpriteID::introdaction_1,SpriteID::introdaction_2,SpriteID::introdaction_3},
+m_tutorial_ids{SpriteID::tutorial_0,SpriteID::tutorial_1,SpriteID::tutorial_2,SpriteID::tutorial_3},
+m_introdaction_passes{"introdaction_0.png","introdaction_1.png","introdaction_2.png","introdaction_3.png"},
+m_tutorial_passes{"tutorial_summons.png","tutorial_choose.png","tutorial_kind.png","tutorial_menuicon.png"}
+{
 
-void TutorialScene::draw(){
-    gl::clear(Color(1,1,1));
-    gl::color(1,1,1);
-      if(is_touch){
-        if(m_manage->Fadeout()){
-            m_manage->changeScene(std::make_shared<mainScene>(m_manage));
-        }
+    is_touch = false;
+    is_end_introdaction = false;
+    current_text_number = 0;
+    m_text_cutpos = Vec2i(0,0);
+    m_text_cutsize = Vec2i(500,1000);
+    m_text_pos = Vec2i(-getWindowWidth() * 0.5f,-getWindowHeight() * 0.5f);
+    m_text_size = Vec2i(getWindowWidth()/2,getWindowHeight()/2);
+    for(int i = 0; i < m_intordaction_ids.size(); i++){
+      resourceManage::getinstace().add(m_intordaction_ids[i],m_introdaction_passes[i]);
+    }
+    for(int i = 0; i < m_tutorial_ids.size();i++){
+        resourceManage::getinstace().add(m_tutorial_ids[i],m_tutorial_passes[i]);
     }
     
 }
 
 void TutorialScene::update(){
+    m_manage->Fadein();
+    setIntordactionText();
+    //ストーリー説明が終わった後、チュートリアル画面へ.
+    if(is_end_introdaction) settutorialText();
+}
+
+void TutorialScene::draw(){
+    gl::clear(Color(1,1,1));
+    gl::color(1,1,1);
+    
+    gl::draw(m_current_text,
+             Area(m_text_cutpos,m_text_cutsize),
+             Area(m_text_pos,m_text_size));
     
 }
 
-void TutorialScene::setText(){
+void TutorialScene::fadeText(){
     
+    
+}
+
+
+void TutorialScene::setIntordactionText(){
+    //ストーリー説明が終わったら、チュートリアルに移るフラグを立てる.
+    if(current_text_number > 3){
+      is_end_introdaction = true;
+      current_text_number = 0;
+    }
+    else{
+      m_current_text = resourceManage::getinstace().getsprite(m_intordaction_ids[current_text_number]);
+    }
+}
+
+//チュートリアル用の画像を準備
+void TutorialScene::settutorialText(){
+    //一番最初の画像だったら、過去の一個前の画像を準備しない.
     m_past_text = (current_text_number == 0)
-    ? resourceManage::getinstace().getsprite(m_ids[0]) :resourceManage::getinstace().getsprite(m_ids[current_text_number - 1]);
-    m_current_text = resourceManage::getinstace().getsprite(m_ids[current_text_number]);
-    m_next_text = resourceManage::getinstace().getsprite(m_ids[current_text_number + 1]);
+    ? resourceManage::getinstace().getsprite(m_tutorial_ids[0]) :resourceManage::getinstace().getsprite(m_tutorial_ids[current_text_number - 1]);
     
-    current_text_number++;
+    m_current_text = resourceManage::getinstace().getsprite(m_tutorial_ids[current_text_number]);
+    m_next_text = resourceManage::getinstace().getsprite(m_tutorial_ids[current_text_number + 1]);
+   
 }
 
-//
-void TutorialScene::mouseDown(cinder::app::MouseEvent event){
-    if(event.LEFT_DOWN)
-        m_manage->changeScene(std::make_shared<mainScene>(m_manage));
-}
+
 
 //app用
 void TutorialScene::touchesBegan(cinder::app::TouchEvent event){
     is_touch = true;
-    setText();
+    current_text_number++;
 }
--- tutorialScene.h
+++ tutorialScene.h
@@ -2,27 +2,35 @@
 
 #include "sceneBase.h"
 #include "resources.h"
-
+#include <vector>
 
 class TutorialScene : public sceneBase{
 public:
     TutorialScene(std::shared_ptr<sceneManage>);
     void draw();
     void update();
-    
-    void setText();
+    void fadeText();
+    void setIntordactionText();
+    void settutorialText();
     void touchesBegan(cinder::app::TouchEvent event);
     void touchesMoved(cinder::app::TouchEvent event){}
     void touchesEnded(cinder::app::TouchEvent event){}
     
-    void mouseDown(cinder::app::MouseEvent event);
+    void mouseDown(cinder::app::MouseEvent event){}
     
 private:
     bool is_touch;
+    bool is_end_introdaction;
+    const std::vector<SpriteID> m_intordaction_ids;
+    const std::vector<SpriteID> m_tutorial_ids;
+    const std::vector<std::string> m_introdaction_passes;
+    const std::vector<std::string> m_tutorial_passes;
+    Vec2i m_text_cutpos;
+    Vec2i m_text_cutsize;
+    Vec2i m_text_pos;
+    Vec2i m_text_size;
     
-    const std::vector<SpriteID> m_ids;
-    const std::vector<std::string> m_passes;
-    
+
     int current_text_number;
     gl::Texture m_past_text;
     gl::Texture m_current_text;
